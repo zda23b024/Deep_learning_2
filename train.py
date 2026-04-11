c7547919dd5a1b3b2e949d30ebeb666ff7e23f4a
zda23b024 <zda23b024@iitmz.ac.in>
2026-04-12 00:29:23 +0530

    Update train.py

--- train.py
+++ train.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
     #train_segmentation(DATA_DIR, epochs=50, batch_size=32, lr=1e-4)
 
     print("🚀 Training Localizer...")
-    train_localizer(DATA_DIR, epochs=50, batch_size=32, lr=5e-5)
+    train_localizer(DATA_DIR, epochs=60, batch_size=32, lr=1e-4)
     
     #print("🚀 Training Segmentation...")
     #train_segmentation(DATA_DIR, epochs=30, batch_size=16, lr=1e-4)
